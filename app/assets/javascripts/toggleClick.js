0b798fcb3dbc822fea1f1d9f58e1b0468e24f87f
Chris Smith <cleesmith2006@gmail.com>
2013-11-13 13:17:57 -0500

    replaced deprecated .toggle with custom .toggleClick in app/assets/javascripts/toggleClick.js; also add select all on Incidents list page

--- app/assets/javascripts/toggleClick.js
+++ app/assets/javascripts/toggleClick.js
@@ -7,25 +7,3 @@ $.fn.toggleClick=function(){
 		iteration= (iteration+1) %functions.length;
 	})
 }
-
-// $.fn.clicktoggle = function(a, b) {
-//     return this.each(function() {
-//         var clicked = false;
-//         $(this).bind("click", function() {
-//             if (clicked) {
-//                 clicked = false;
-//                 return b.apply(this, arguments);
-//             }
-//             clicked = true;
-//             return a.apply(this, arguments);
-//         });
-//     });
-// };
-
-// function odd() {
-//     alert("odd");
-// }
-// 
-// function even() {
-//     alert("even");
-// }
